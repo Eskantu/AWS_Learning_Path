b4adb82e0f95998652a83c8e7a27af125bbaac02
Mario Escalante Cantu <eskantu@hotmail.com>
2026-03-16 22:50:13 -0600

    Add: lambda worker
    Creacion de proyecto lambda para poder procesar mensaje proveniente de SQS

---
+++ EventDriven/src/FileProcessingWorker/Services/FileProcessingService.cs
@@ -0,0 +1,54 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace FileProcessingWorker.Services;
+
+public class FileProcessingService
+{
+    private readonly IAmazonS3 _s3Client;
+    private readonly IAmazonDynamoDB _dynamoDb;
+
+    public FileProcessingService(IAmazonS3 s3Client, IAmazonDynamoDB dynamoDb)
+    {
+        _s3Client = s3Client;
+        _dynamoDb = dynamoDb;
+    }
+
+    public async Task ProcessFile(string bucket, string key)
+    {
+        using GetObjectResponse response = await _s3Client.GetObjectAsync(bucket, key);
+
+        await SaveMetadata(bucket, key, response.ContentLength);
+    }
+
+    private async Task SaveMetadata(string bucket, string filename, long contentLength)
+    {
+        string objectKey = $"{bucket}/{filename}";
+
+        var request = new PutItemRequest
+        {
+            TableName = "file-metadata-serverless",
+            ConditionExpression = "attribute_not_exists(Id)",
+            Item = new Dictionary<string, AttributeValue>
+            {
+                {"Id", new AttributeValue { S = objectKey }},
+                {"FileName", new AttributeValue { S = filename }},
+                {"Bucket", new AttributeValue { S = bucket }},
+                {"FileSize", new AttributeValue { N = contentLength.ToString() }},
+                {"UploadTime", new AttributeValue { S = DateTime.UtcNow.ToString("o") }}
+            }
+        };
+
+        try
+        {
+            await _dynamoDb.PutItemAsync(request);
+        }
+        catch (ConditionalCheckFailedException)
+        {
+            // duplicate event ignored
+
+        }
+    }
+}
